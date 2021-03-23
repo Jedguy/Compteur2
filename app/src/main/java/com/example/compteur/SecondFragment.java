4cdb533f8c4e0a6227f0d9f35f43025bfe031ed8
kenan <kenan@DESKTOP-VN48SV7>
2021-03-23 07:44:20 +0100

    Commit, chercher la liaison pour les boutons

--- app/src/main/java/com/example/compteur/SecondFragment.java
+++ app/src/main/java/com/example/compteur/SecondFragment.java
@@ -16,12 +16,14 @@ import androidx.navigation.fragment.NavHostFragment;
 public class SecondFragment extends Fragment {
 
     public int compteur = 0;
-    //labite
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         TextView textView = (TextView) findViewById(R.id.textView);
         return inflater.inflate(R.layout.fragment_second, container, false);
+    }
+
+    private Object findViewById(int textView) {
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
@@ -39,25 +41,26 @@ public class SecondFragment extends Fragment {
             @Override
             public void onClick(View view) {
                 String valeur = textView.getText().toString();
-                compteur = compteur + Integer.parseInt(valeur);
-                NavHostFragment.findNavController(SecondFragment.this)
-                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
+                compteur = Integer.parseInt(valeur) - 1;
+                textView.setText("" + compteur);
             }
         });
 
         view.findViewById(R.id.buttonPlus).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(SecondFragment.this)
-                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
+                String valeur = textView.getText().toString();
+                compteur = Integer.parseInt(valeur) + 1;
+                textView.setText("" + compteur);
             }
         });
 
         view.findViewById(R.id.buttonRAZ).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                NavHostFragment.findNavController(SecondFragment.this)
-                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
+                String valeur = textView.getText().toString();
+                compteur = 0;
+                textView.setText("" + compteur);
             }
         });
 }
